920ca54c5cb44c9bd1849cd39eacf6246fcfa99f
Yulong Tan <yulongtan67@gmail.com>
2017-06-20 14:06:54 -0700

    initial commit

---
+++ DocumentDBDemo/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace DocumentDBDemo {
+    class MainClass {
+        public static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
